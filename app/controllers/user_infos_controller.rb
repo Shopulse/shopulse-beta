71bfe1ff85f36bba92307cd96df0e4a009b350e5
Michael Go <ggmichaelgo@gmail.com>
2013-06-11 01:19:45 -0300

    fixed update profile bug

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -4,11 +4,14 @@ class UserInfosController < ApplicationController
 	end
 
 	def update
-		@user_info = current_user.user_info
+		user_info = current_user.user_info
 		respond_to do |format|
-			if @user_info.update_attributes(params[:user_info])
-				format.html { redirect_to :action => 'edit_profile' , notice: 'Successfully Updated! 10/10!' }
-				redirect_to :controller => "products", :action => "index"
+			
+			if user_info.update_attributes params[:user_info]
+				format.html { 
+					#redirect_to :action => 'edit_profile' , notice: 'Successfully Updated! 10/10!' 
+					redirect_to :controller => "products", :action => "index"
+				}
 			else
 				format.html { render action: "edit_profile" }
 			end
