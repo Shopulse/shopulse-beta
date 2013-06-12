95296beb18550f121532570ba53022f561c11e9a
Michael Go <ggmichaelgo@gmail.com>
2013-06-12 18:46:47 -0300

    fixed empty image file bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,9 @@ class ProductsController < ApplicationController
     @product = user.products.find(params[:id]) if !user.admin
     @product = Product.find(params[:id]) if user.admin
     (5-@product.photos.count).times { @product.photos.build }
+    @product.photos.each do |x|
+      x.destroy if x.photo_file_name == nil
+    end
   end
 
   # POST /products
