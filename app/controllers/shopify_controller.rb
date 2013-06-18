63d3bac36e626641dd5d33ba80e9b465f94ddf6c
Michael Go <ggmichaelgo@gmail.com>
2013-06-18 01:58:36 -0300

    working on shopify webhook

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -1,9 +1,11 @@
 class ShopifyController < ApplicationController
+	skip_before_filter :authenticate
+	
 	@@temp = 1
 	def check_out_create
 		@@temp = params
 		render :json => @@temp
-		10.times { puts params }
+		10.times { puts params 	}
 	end
 
 	def check
