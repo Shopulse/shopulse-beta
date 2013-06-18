f245c4113e7dcc482733517a7905f60a856a6bdd
Michael Go <ggmichaelgo@gmail.com>
2013-06-18 01:40:39 -0300

    testing shopify API

---
+++ app/controllers/shopify_controller.rb
@@ -0,0 +1,11 @@
+class ShopifyController < ApplicationController
+	@@temp = 1
+	def check_out_create
+		@@temp = params
+		render :json => @@temp 
+	end
+
+	def check
+		render :json => @@temp
+	end
+end
