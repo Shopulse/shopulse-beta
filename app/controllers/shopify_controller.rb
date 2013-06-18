00dabf8017f61e2258263330b5d6ccf90232cb9a
Michael Go <ggmichaelgo@gmail.com>
2013-06-18 03:07:00 -0300

    Shopify updates the Shopulse's inventory when there is a purchase

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -1,21 +1,32 @@
 class ShopifyController < ApplicationController
 	skip_before_filter :authenticate
 
-	def check_out_create
+	def order_create
+		update_list
+	end
+
+	def order_cancel
+		update_list
+	end
+
+	private
+	def update_list
 		checkout = params
 		list = checkout.line_item.map { |x| x.product_id }
 		list.uniq!
 
 		list.each do |shopify_id|
-			p = Product.where "shopify_id = #{shopify_id}"
-			sizes = {}
-			p.variants.each do |x| 
-				sizes[title] = x.inventory_quantity
-			end
-			p.sizes = sizes.to_s
-			p.save
+			update_product shopify_id
 		end
 	end
 
-
+	def update_product shopify_id
+		p = Product.where "shopify_id = #{shopify_id}"
+		sizes = {}
+		p.variants.each do |x| 
+			sizes[title] = x.inventory_quantity
+		end
+		p.sizes = sizes.to_s
+		p.save
+	end
 end
