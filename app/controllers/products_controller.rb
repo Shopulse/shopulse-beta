f383ac6e22b95907d92974880ac174db2291ab06
Michael Go <ggmichaelgo@gmail.com>
2013-07-09 00:08:37 -0300

    small patch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,140 +1,146 @@
 class ProductsController < ApplicationController
-  # GET /products
-  # GET /products.json
-  def index
-    user = current_user.user_info    
-    @products = user.products
-    if user.admin
-      @products = Product.all 
-      @admin = true
-    end
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @products }
-    end
-  end
+	# GET /products
+	# GET /products.json
+	def index
+		user = current_user.user_info    
+		@products = user.products
+		if user.admin
+			@products = Product.all 
+			@admin = true
+		end
 
-  # GET /products/1
-  # GET /products/1.json
-  def show    
-    user = current_user.user_info
-    product = user.products.find(params[:id]) if !user.admin
-    product = Product.find(params[:id]) if user.admin
-    # respond_to do |format|
-    #   format.html # show.html.erb
-    #   format.json { render json: @product }
-    # end
-    p = ShopifyAPI::Product.find product.shopify_id
-    redirect_to 'http://shopulse.myshopify.com/products/'+p.handle
-  end
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json { render json: @products }
+		end
+	end
 
-  # GET /products/new
-  # GET /products/new.json
-  def new
-    @product = Product.new
-    5.times { @product.photos.build }
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @product }
-    end    
-  end
+	# GET /products/1
+	# GET /products/1.json
+	def show    
+		user = current_user.user_info
+		product = user.products.find(params[:id]) if !user.admin
+		product = Product.find(params[:id]) if user.admin
+		p = ShopifyAPI::Product.find product.shopify_id
+		redirect_to 'http://shopulse.myshopify.com/products/'+p.handle
+	end
 
-  # GET /products/1/edit
-  def edit
-    user = current_user.user_info
-    @product = user.products.find(params[:id]) if !user.admin
-    @product = Product.find(params[:id]) if user.admin
-    photo_count = @product.photos.count
-    
-    (5-photo_count).times { @product.photos.build }
-    @product.photos.each do |x|      
-      x.destroy if x.photo_file_name == nil
-      puts @product.photos.count
-    end
-  end
+	# GET /products/new
+	# GET /products/new.json
+	def new
+		@product = Product.new
+		5.times { @product.photos.build }
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render json: @product }
+		end    
+	end
 
-  # POST /products
-  # POST /products.json
-  def create
-    remove_empty_image params[:product][:photos_attributes]
-    
-    user_info = current_user.user_info
-    if user_info.admin
-      user_info = UserInfo.find params[:product][:user_id].to_i
-      params[:product].delete :user_id
-    end
+	# GET /products/1/edit
+	def edit
+		user = current_user.user_info
+		@product = user.products.find(params[:id]) if !user.admin
+		@product = Product.find(params[:id]) if user.admin
+		photo_count = @product.photos.count
 
-    @product = Product.new(params[:product])        
-    respond_to do |format|
-      if @product.save
-        user_info.products.push @product
-        Shopify.create @product
-        format.html { redirect_to :action => 'index' }
-        format.json { render json: @product, status: :created, location: @product }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		(5-photo_count).times { @product.photos.build }
+		@product.photos.each do |x|      
+			x.destroy if x.photo_file_name == nil
+			puts @product.photos.count
+		end
+	end
 
-  # PUT /products/1
-  # PUT /products/1.json
-  def update
-    user = current_user.user_info
-    product = user.products.find(params[:id]) if !user.admin
-    product = Product.find(params[:id]) if user.admin
-    (5-product.photos.count).times { product.photos.build }
-    
-    remove_empty_image params[:product][:photos_attributes]
+	# POST /products
+	# POST /products.json
+	def create
+		remove_empty_image params[:product][:photos_attributes]
 
-    respond_to do |format|
-      if product.update_attributes(params[:product])
-        product.photos.delete_if { |x| x.photo_file_name == nil }
-        puts product.photos
-        product.save
-        Shopify.modify product
-        format.html { redirect_to :action => 'index' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		user_info = current_user.user_info
+		admin = false
+		if user_info.admin
+			user_info = UserInfo.find params[:product][:user_id].to_i
+			params[:product].delete :user_id
+			admin = true
+		end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
-  def destroy    
-    user = current_user.user_info
-    product = user.products.find(params[:id]) if !user.admin
-    product = Product.find(params[:id]) if user.admin
-    Shopify.delete product
-    product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url }
-      format.json { head :no_content }
-    end
-  end
+		@product = Product.new(params[:product])        
+		respond_to do |format|
+			if @product.save
+				user_info.products.push @product
+				Shopify.create @product
+				if admin == true
+					format.html { redirect_to :action => 'retailer', :id => user_info.id }  
+				else          
+					format.html { redirect_to :action => 'index' }
+				end
+				format.json { render json: @product, status: :created, location: @product }
+			else
+				format.html { render action: "new" }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  def retailer
-    user = current_user.user_info
-    redirect_to "/" if !user.admin
-    @products = UserInfo.find(params[:id]).products
-    @admin = true
-    render :action => "index"
-  end
+	# PUT /products/1
+	# PUT /products/1.json
+	def update
+		user = current_user.user_info
+		product = user.products.find(params[:id]) if !user.admin
+		if user.admin
+			product = Product.find(params[:id]) 
+			params[:product].delete :user_id
+		end
+		(5-product.photos.count).times { product.photos.build }
 
-  private
-  def remove_empty_image list
-    #remove empty pictures
-    list.each do |x|
-      begin
-        list.delete x[0] if x[1]["photo"] == ""
-      rescue
-        #nothting
-      end
-    end
-  end
+		remove_empty_image params[:product][:photos_attributes]
+
+		respond_to do |format|
+			if product.update_attributes(params[:product])
+				product.photos.delete_if { |x| x.photo_file_name == nil }
+				product.save
+				Shopify.modify product
+				if 
+					format.html { redirect_to :action => 'index' }
+					format.json { head :no_content }
+				else
+					format.html { render action: "edit" }
+					format.json { render json: product.errors, status: :unprocessable_entity }
+				end
+			end
+		end
+	end
+
+	# DELETE /products/1
+	# DELETE /products/1.json
+	def destroy    
+		user = current_user.user_info
+		product = user.products.find(params[:id]) if !user.admin
+		product = Product.find(params[:id]) if user.admin
+		Shopify.delete product
+		product.destroy
+		respond_to do |format|
+			format.html { redirect_to products_url }
+			format.json { head :no_content }
+		end
+	end
+
+	def retailer
+		user = current_user.user_info
+		redirect_to "/" if !user.admin
+		@products = UserInfo.find(params[:id]).products
+		@admin = true
+		render :action => "index"
+	end
+	
+	def remove_empty_image list
+		#remove empty pictures
+		list.each do |x|
+			begin
+				list.delete x[0] if x[1]["photo"] == ""
+			rescue
+				puts "error"
+				puts list
+			end
+		end
+	end
 end
